c82706b5aade60b0f8d8da99e3aa968913b03217
Indranil Gayen <nil.gayen@gmail.com>
2019-08-19 16:54:59 +0530

    Ready for CRAN - this is also test

--- test/winUpload.R
+++ test/winUpload.R
@@ -5,6 +5,6 @@ if(!requireNamespace("pkgbuild", quietly = TRUE)) install.packages("pkgbuild")
 built_path <- pkgbuild::build()
 
 links <- paste0("ftp://win-builder.r-project.org/", c("R-devel", "R-release", "R-oldrelease"), "/", basename(built_path))
-
-lapply(links, RCurl::ftpUpload, what = built_path)
+fup <- function(url) {try(RCurl::ftpUpload(what = built_path, to = url), silent = TRUE)}
+lapply(links, fup)
 
