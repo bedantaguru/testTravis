5ea730f47b9ecf56d4e3803c99d3d3e4281d5523
Indranil Gayen <nil.gayen@gmail.com>
2019-08-19 17:15:02 +0530

    Ready for CRAN - Added `winbuilder` - Fix for nacnudus/unpivotr#26

--- test/winUpload.R
+++ test/winUpload.R
@@ -1,10 +1,22 @@
 
 if(!requireNamespace("RCurl", quietly = TRUE)) install.packages("RCurl")
-if(!requireNamespace("pkgbuild", quietly = TRUE)) install.packages("pkgbuild")
 
-built_path <- pkgbuild::build()
+built_path <- list.files(pattern = "testTravis_[0-9.]+.tar.gz", full.names = T)
+if(length(built_path)>0){
+  built_path <- normalizePath(max(built_path))
+  links <- paste0("ftp://win-builder.r-project.org/", c("R-devel", "R-release", "R-oldrelease"), "/", basename(built_path))
+  fup <- function(url) {try(RCurl::ftpUpload(what = built_path, to = url), silent = TRUE)}
+  lapply(links, fup)
+}
 
-links <- paste0("ftp://win-builder.r-project.org/", c("R-devel", "R-release", "R-oldrelease"), "/", basename(built_path))
-fup <- function(url) {try(RCurl::ftpUpload(what = built_path, to = url), silent = TRUE)}
-lapply(links, fup)
+'built_path <- list.files(pattern = "testTravis_[0-9.]+.tar.gz", full.names = T)
+if(length(built_path)>0){
+  built_path <- normalizePath(max(built_path))
+  links <- paste0("ftp://win-builder.r-project.org/", c("R-devel", "R-release", "R-oldrelease"), "/", basename(built_path))
+  fup <- function(url) {try(RCurl::ftpUpload(what = built_path, to = url), silent = TRUE)}
+  lapply(links, fup)
+}' -> x
 
+require(stringr)
+
+x %>% str_split("\n") %>% unlist() %>% str_trim() %>% paste0(collapse = ";") %>% cat()
